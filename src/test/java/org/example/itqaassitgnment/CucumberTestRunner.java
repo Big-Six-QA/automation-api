d3b13b3e71bf4f297f3e3a37a794c0c6f4bb0794
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-01-03 14:43:37 +0530

    add features path to test runner class

--- src/test/java/org/example/itqaassitgnment/CucumberTestRunner.java
+++ src/test/java/org/example/itqaassitgnment/CucumberTestRunner.java
@@ -7,8 +7,10 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {
-                "src/test/resources/features/admin-get-books.feature",
-                "src/test/resources/features/user-get-books.feature",
+                "src/test/resources/features/admin-create-book.feature",
+                "src/test/resources/features/admin-get-book.feature",
+                "src/test/resources/features/user-create-book.feature",
+                "src/test/resources/features/user-get-book.feature",
         },
         glue = "org.example.itqaassitgnment",
         plugin = {"pretty", "html:target/cucumber-reports.html"},
